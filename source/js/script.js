0c461500767abf8f804bffc4f41527941f1d35f1
Elena Ordina <post032@yandex.ru>
2018-11-21 16:58:10 +0300

    убрано br из комментариев

--- source/js/script.js
+++ source/js/script.js
@@ -1,25 +1,3 @@
-// var button = document.querySelector(".goods-week__link");
-// var modal = document.querySelector(".modal");
-//
-// button.addEventListener("click", function(evt) {
-//   evt.preventDefault();
-//   modal.classList.toggle("modal-close");
-//   arrival.focus();
-// });
-
-// var link = document.querySelector(".goods-week__link");
-//
-// var popup = document.querySelector(".modal");
-//  var form = popup.querySelector("form");
-// link.addEventListener("click", function (evt) {
-//   evt.preventDefault();
-//   popup.classList.add("modal-show");
-// });
-// form.addEventListener("submit", function (evt) {
-//   evt.preventDefault();
-// });
-
-
 
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
