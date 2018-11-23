fa2a2cef0551b08754c6d7af42012cd614931fe6
Elena Ordina <post032@yandex.ru>
2018-11-23 10:54:54 +0300

    завершение работы над модальным окном

--- source/js/script.js
+++ source/js/script.js
@@ -1,30 +1,29 @@
 
-var navMain = document.querySelector('.main-nav');
-var navToggle = document.querySelector('.main-nav__toggle');
+var navMain = document.querySelector(".main-nav");
+var navToggle = document.querySelector(".main-nav__toggle");
 
-navMain.classList.remove('main-nav--nojs');
+navMain.classList.remove("main-nav--nojs");
 
-navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
+navToggle.addEventListener("click", function() {
+  if (navMain.classList.contains("main-nav--closed")) {
+    navMain.classList.remove("main-nav--closed");
+    navMain.classList.add("main-nav--opened");
   } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
+    navMain.classList.add("main-nav--closed");
+    navMain.classList.remove("main-nav--opened");
   }
 });
 
 
 var link = document.querySelector(".goods-week__link, .catalog__icon");
 var popup = document.querySelector(".modal");
-var close = popup.querySelector(".modal__add");
-var size = popup.querySelector("[name=button1]");
+// var close = popup.querySelector(".modal::after");
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.add("modal-show");
-  size.focus();
 });
 
 close.addEventListener("click", function (evt) {
+    evt.preventDefault();
     popup.classList.remove("modal-show");
 });
